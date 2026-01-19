7a034ced85579fb3c5b742cb692d2be3ad75e9ea
sconezipsa <sconezipsa@gmail.com>
2026-01-19 09:58:34 +0900

    [프로그래머스] L1 - 서울에서 김서방 찾기 (#237)

---
+++ programmers/L1_입문/237_서울에서_김서방_찾기.py
@@ -0,0 +1,11 @@
+# 서울에서 김서방 찾기
+# 프로그래머스 L1 (입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12919
+# 알고리즘: 배열, 탐색
+# 작성자: 정서영
+# 작성일: 2026. 01. 19. 09:58:33
+
+def solution(seoul):
+    x = seoul.index("Kim")
+    answer = (f"김서방은 {x}에 있다")
+    return answer
