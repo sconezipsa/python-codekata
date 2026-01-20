443bb8ecc3d840110ecae995d4adf364b09d51a2
sconezipsa <sconezipsa@gmail.com>
2026-01-20 14:29:40 +0900

    [프로그래머스] L2 - x만큼 간격이 있는 n개의 숫자 (#229)

--- programmers/L2_기초/229_x만큼_간격이_있는_n개의_숫자.py
+++ programmers/L2_기초/229_x만큼_간격이_있는_n개의_숫자.py
@@ -3,10 +3,10 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12954
 # 알고리즘: 배열
 # 작성자: 정서영
-# 작성일: 2026. 01. 20. 14:16:16
+# 작성일: 2026. 01. 20. 14:29:39
 
-def solution(x, n):
-    answer = []
-    for i in range(1, n+1):
-        answer.append(x * i)
+def solution(x,n):
+    answer=[]
+    for i in range(1,n+1):
+        answer.append(x*i)
     return answer
