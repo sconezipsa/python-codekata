0409b43793f4f32b55c86b023c164bad24dc239e
sconezipsa <sconezipsa@gmail.com>
2026-02-05 09:25:22 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,10 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 정서영
+# 작성일: 2026. 02. 05. 09:25:21
+
+def solution(n):
+    answer = (n+6)//7
+    return answer
